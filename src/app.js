591be45669cbe40e06f58e037ed7e9a4ddf5e807
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2022-09-07 15:49:56 +0200

    adding todos to the DOM

--- src/app.js
+++ src/app.js
@@ -1,40 +1,52 @@
 // SELECTING THE FORM ELEMENTS
 const form = document.getElementById('todoform')
 const todoInput = document.getElementById('newtodo')
+const todoListElements = document.getElementById('todos-list')
 
-// ARRAY FOR SAVING TODOS
+// ARRAY FOR ---SAVING TODOS---
 let todos = [];
 
 // FORM SUBMIT PREVENT PAGE FROM REFRESHING ON NORMAL SUBMIT
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
-
+  
   saveTodo();
+  renderTodos();
 });
 
 // SAVING TODO'S
- function saveTodo(){
+function saveTodo() {
   const todoValue = todoInput.ariaValue
 
+  //ADDING ERRORS
   //CHECK IF THE TODO IS EMPTY SHOW ERROR
-const isEmpty = todoValue === '';
+  const isEmpty = todoValue === '';
 
-//CHECK FOR DUPLICATE TODO'S SHOW ERROR
-const isDuplicate =
-todos.some((todo) => todo.value === todoValue);
+  //CHECK FOR DUPLICATE TODO'S SHOW ERROR
+  const isDuplicate =
+    todos.some((todo) => todo.value.toUpperCase() === todoValue.toUpperCase());
 
-if(isEmpty){
-  alert("todo's input is empty");
-}else{const todo = {
-  value : todoValue,
-  checked : false,
+  if (isEmpty) {
+    alert("todo's input is empty");
+  } else if(isDuplicate){
+    alert('todo already exists')
+  }
+  else {    
+    todos.push({
+      value: todoValue,
+      checked: false,
+
   //RANDOMIZING EACH TODO COLOR ON LIST
-  color : '#' + Math.floor(Math.random()*16777215).toString(16)
+      color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+    });
+
+  //CLEAR FIELD AFTER SUBMITTING TODO
+    todoInput.value = '';
+  }
 }
 
-todos.push(todo)
+// RENDERING TODO'S ON DOM  
+function renderTodos(){
 
 }
- }
-
 
